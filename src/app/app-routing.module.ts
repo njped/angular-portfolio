b0b8de8c59f53dac51f0c6b657ee009e525a388f
Nathan Pedigo <njpedigo@gmail.com>
2023-09-06 14:45:42 -0600

    created routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { SearchComponent } from './search/search.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'about', component: AboutMeComponent},
+  { path: 'search', component: SearchComponent},
+  { path: 'portfolio', component: PortfolioComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
